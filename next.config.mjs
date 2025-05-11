720c4605a68f1f4b688f6c77e4512112a53df7a7
silvio <silviongougni139@gmail.com>
2025-05-11 15:56:58 +0200

    Add img.png

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['files.edgestore.dev'],
+  },
   typescript: {
+    // ⚠️ Ignore les erreurs TypeScript au build (à utiliser avec prudence)
     ignoreBuildErrors: true,
   },
   eslint: {
+    // ⚠️ Ignore les erreurs ESLint au build
     ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ['your-edgestore-bucket-name.edgestore.dev'], // 🔁 Remplace par le vrai domaine EdgeStore
   },
 }
 
